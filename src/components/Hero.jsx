7f876beca50845b59c45dcef724964897c4f6d06
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2024-06-04 19:02:06 +0530

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero()  {
     <section className=' md:cursor-none'  >
     <BackgroundGradientAnimation>
       <div className="absolute z-30 inset-0 flex flex-col items-center justify-center font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <div className="lg:tracking-widest text-xs tracking-wider lg:mx-4 pb-5"> 
+        <div className="lg:tracking-widest text-xs md:text-base tracking-wider pb-5"> 
           TECHNICAL FESTIVAL
         </div>
         <div className="text-5xl md:text-7xl font-extrabold tracking-wide py-5">
