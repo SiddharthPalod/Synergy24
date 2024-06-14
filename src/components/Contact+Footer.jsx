43b9134a5ab1a5db180bd0c2ab6f0111c4ab0233
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2024-06-14 17:24:47 +0530

    Add files via upload

--- src/components/Contact+Footer.jsx
+++ src/components/Contact+Footer.jsx
@@ -6,13 +6,13 @@ import { contact_iiitb, contact_syn } from '../assets';
 function Footer() {
 return (
     <section id='contact'  className="flex flex-col items-center justify-end h-full" style={{backgroundColor:"#0075FF"}} >
-        <footer className="footer footer-center p-6 bg-none text-white" >
+        <footer className="footer footer-center p-8 bg-none text-white" >
         <div
             className="text-5xl md:text-7xl font-extrabold md:tracking-wide line-clamp-2">
                 Contact Us 
         </div>
         <nav>
-            <div className="grid grid-flow-col gap-4 scale-150">
+            <div className="grid grid-flow-col gap-4 scale-150 z-20">
                 <a href="mailto:synergy@iiitb.ac.in" className='hover:-translate-y-1'>
                     <Icon icon="ri:mail-fill" width="1.3rem" height="1.3rem"/>
                 </a>
@@ -27,7 +27,7 @@ return (
                 </a>
             </div>
         </nav>
-        <aside className='flex flex-row justify-between'>
+        <aside className='min-[890px]:absolute mx-4 flex flex-row justify-between'>
              <div className="flex-initial w-1/3 md:w-1/4  flex justify-center items-center"><img src={contact_syn} alt="Logo"/></div>
              <div className="flex-initial w-1/3 md:w-1/4 flex justify-center items-center"><img src={contact_iiitb} alt="Logo" /></div>
         </aside> 
