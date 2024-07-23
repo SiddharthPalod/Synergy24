7b8ed0078c8da296397bc4eb807b1da5f1668dc9
vaibhav bajoriya <Bajoriya.Vaibhav@Outlook.comm>
2024-07-23 20:37:18 +0530

    Responsive mark

--- src/components/BookMark.jsx
+++ src/components/BookMark.jsx
@@ -5,7 +5,7 @@ import { Book3 } from '../assets/index'
 const BookMark = () => {
   return (
     <div className='absolute bottom-10 right-10 z-10'>
-      <img src={Book3} alt="book" className="w-20 h-20"/>
+      <img src={Book3} alt="book" className="w-[15vw] sm:w-[10vw] lg:w-[5vw]"/>
     </div>
   )
 }
