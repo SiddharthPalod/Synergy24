76e137a2f4ab048733762d9353daf16543013d57
vaibhav bajoriya <Bajoriya.Vaibhav@Outlook.comm>
2024-08-15 17:11:16 +0530

    navbar fixed

--- src/components/BookMark.jsx
+++ src/components/BookMark.jsx
@@ -4,7 +4,7 @@ import {book2 } from '../assets'
 export const Bookmark = () => {
   return (
     <a href='https://drive.google.com/file/d/15eoag6GCNBWqgCOsAR_Zbt8kFVZ0qSWT/view' target="_blank" className='bg-white rounded-full p-2 fixed bottom-10 right-10 z-10'>
-      <img src={book2} alt="book" className="w-[15vw] sm:w-[10vw] lg:w-[5vw]"/>
+      <img src={book2} alt="book" className="w-[10vw] sm:w-[7vw] lg:w-[5vw]"/>
     </a>
   )
 }
