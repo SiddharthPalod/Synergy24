29e6a9c08abe4e146979a57dcce96dd9dc1175e3
vaibhav bajoriya <Bajoriya.Vaibhav@Outlook.comm>
2024-09-06 13:51:27 +0530

    new sponsors added

--- src/assets/company_logos/index.js
+++ src/assets/company_logos/index.js
@@ -1,6 +1,9 @@
 import InterviewCake from './interviewcake.png';
 import Wolfram from './wolfram.png';
 import BlockChainedIndia from './blockchainedIndia.png';
-const company_logos = [InterviewCake, Wolfram, BlockChainedIndia, InterviewCake, Wolfram, BlockChainedIndia];
+import Polygon from './polygon.png';
+import EthIndia from './ethIndia.png';
+import Devfolio from './devfolio.png';
+const company_logos = [InterviewCake, Wolfram, BlockChainedIndia, Polygon, EthIndia,Devfolio];
 
 export default company_logos;
