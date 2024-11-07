4821d86301669549713c6cfe246d5b85737f010b
siddharthpalod <siddharthpalod@gmail.com>
2024-11-07 15:09:18 +0530

    lts

--- src/assets/company_logos/index.js
+++ src/assets/company_logos/index.js
@@ -8,6 +8,6 @@ import Balsamiq from './balsamiq.png'
 import CTR from './CTR.svg';
 import Foundation from './Foundation.svg'
 import AKC from './akc.jpg'
-const company_logos = [{img:InterviewCake,link:'https://www.interviewcake.com'}, {img:Wolfram,link:'https://www.wolfram.com/wolfram-one/'}, {img:Quillbot,link:'http://quillbot.com/'}, {img:Languagetool,link:'https://languagetool.org/'},{img:Axure,link:'https://www.axure.com'}, {img:Niia,link:'https://niia.ai/'}, {img:Balsamiq,link:'https://balsamiq.com/'}, {img:AKC,link:'https://www.akccapital.com/'}, {img:CTR,link:'https://www.iiitb.ac.in/media/iiit-bangalore-announces-gok-funded-digital-governance-and-datadriven-planning-initiatives'}, {img:Foundation,link:'https://www.mphasis.com/home/corporate/community-social-responsibility.html'}];
+const company_logos = [{img:InterviewCake,link:'https://www.interviewcake.com'}, {img:Wolfram,link:'https://www.wolfram.com/wolfram-one/'}, {img:Quillbot,link:'http://quillbot.com/'}, {img:Languagetool,link:'https://languagetool.org/'},{img:Axure,link:'https://www.axure.com'}, {img:Niia,link:'https://niia.ai/?utm_source=IIITBSynergy&utm_medium=IIITBSynergy&utm_campaign=IIITBSynergy'}, {img:Balsamiq,link:'https://balsamiq.com/'}, {img:AKC,link:'https://www.akccapital.com/'}, {img:CTR,link:'https://www.iiitb.ac.in/media/iiit-bangalore-announces-gok-funded-digital-governance-and-datadriven-planning-initiatives'}, {img:Foundation,link:'https://www.mphasis.com/home/corporate/community-social-responsibility.html'}];
 
 export default company_logos;
